28889da7eaba87667494275f20f808dd14b3d254
Robert van Kempen <DoNormal@users.noreply.github.com>
2019-12-30 16:21:20 +0100

    fix: add favicons

--- components/layout/head.tsx
+++ components/layout/head.tsx
@@ -18,6 +18,32 @@ export const Head = (props: Props) => {
             {props.description ? (
                 <meta name="description" content={props.description} />
             ) : null}
+
+            <link
+                rel="apple-touch-icon"
+                sizes="180x180"
+                href="/apple-touch-icon.png"
+            />
+            <link
+                rel="icon"
+                type="image/png"
+                sizes="32x32"
+                href="/favicon-32x32.png"
+            />
+            <link
+                rel="icon"
+                type="image/png"
+                sizes="16x16"
+                href="/favicon-16x16.png"
+            />
+            <link rel="manifest" href="/site.webmanifest" />
+            <link
+                rel="mask-icon"
+                href="/safari-pinned-tab.svg"
+                color="#ffa100"
+            />
+            <meta name="msapplication-TileColor" content="#ffa100" />
+            <meta name="theme-color" content="#ffa100" />
         </NextHead>
     )
 }

b902719451f92e39cc54950e6547a9caf90ee7c4
Robert van Kempen <DoNormal@users.noreply.github.com>
2019-12-30 14:43:27 +0100

    fix: add titles

--- components/layout/_introduction.tsx
+++ components/layout/_introduction.tsx
@@ -1,10 +1,19 @@
 import { MDXProvider } from '@mdx-js/react'
 import { Navbar } from './navbar'
 import { markdownComponents } from '@beequip/dev-lib/mdx-components'
+import { Head } from '@beequip/dev-components/layout/head'
 
 export const Introduction = props => {
+    const { meta } = props
+
     return (
         <MDXProvider components={{ ...markdownComponents }}>
+            <Head
+                title={meta.title}
+                description={meta.description}
+                titlePrefix=""
+                titleSuffix="– Beequip Developer"
+            />
             <Navbar />
             {props.children}
         </MDXProvider>
